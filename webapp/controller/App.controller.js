ad446335c2d9fa042b1d9dd85f8efffedc168225
Matej Petrić <matej.petric@msan.hr>
2023-02-10 12:51:43 +0100

    Step 13: Element Binding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,13 +5,16 @@ sap.ui.define([
     "sap/ui/core/LocaleData",
     "sap/ui/model/type/Currency"
 ], function (Controller, mobileLibrary, Locale, LocaleData, Currency) {
-    'use strict';
+    "use strict"
 
     return Controller.extend("sap.ui.demo.db.controller.App", {
         formatMail: function (sFirstName, sLastName) {
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            return mobileLibrary.URLHelper.normalizeEmail(sFirstName + "." + sLastName + "@example.com", oBundle.getText("mailSubject", [sFirstName]),
-                oBundle.getText("mailBody"));
+            return mobileLibrary.URLHelper.normalizeEmail(
+                sFirstName + "." + sLastName + "@example.com",
+                oBundle.getText("mailSubject", [sFirstName]),
+                oBundle.getText("mailBody")
+            );
         },
         formatStockValue: function (fUnitPrice, iStockLevel, sCurrCode) {
             var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
@@ -19,8 +22,13 @@ sap.ui.define([
             var oLocaleData = new LocaleData(oLocale);
             var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
             return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
-
+        },
+        onItemSelected: function (oEvent) {
+            var oSelectedItem = oEvent.getSource();
+            var oContext = oSelectedItem.getBindingContext("products");
+            var sPath = oContext.getPath();
+            var oProductDetailPanel = this.byId("productDetailsPanel");
+            oProductDetailPanel.bindElement({ path: sPath, model: "products" });
         }
-
-    });
-});
+    })
+})
